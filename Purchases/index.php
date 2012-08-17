233a9801980b3458a7299df4af2a938ac3d067da
kpurdie-cc <kpurdie-cc@CCWAT2D04STAC02.conestogac.on.ca>
2012-08-17 11:55:13 -0400

    adding database on the code

--- Purchases/index.php
+++ Purchases/index.php
@@ -1,4 +1,17 @@
 <?php
+require '../ActiveRecord/ActiveRecord.php';
+
+ActiveRecord\Config::initialize(function($cfg)
+{
+	$cfg->set_model_directory('model');
+	$cfg->set_connections(
+			array(
+					'development' => 'mysql://root:@localhost/purchases',//name of the database
+					'test' => 'mysql://username:password@localhost/test_database_name',
+					'production' => 'mysql://username:password@localhost/production_database_name'
+			)
+	);
+});
 ?>
 <html><body>
 Hello World!
