39e93320f476fa119579804a4bf40b93d15619a9
kpurdie-cc <kpurdie-cc@CCWAT2D04STAC02.conestogac.on.ca>
2012-08-20 09:21:38 -0400

    adding Exam1 to git

--- Purchases/index.php
+++ Purchases/index.php
@@ -12,7 +12,21 @@ ActiveRecord\Config::initialize(function($cfg)
 			)
 	);
 });
+if(array_key_exists('submit', $_POST)){
+	
+	$oPurchase = new Purchase;//becasue I have no parameters other wise catchers require.
+	$oPurchase->date = $_POST['date'];
+	$oPurchase->purchase = $_POST['purchase'];
+	$oPurchase->price = $_POST['price'];
+	$oPurchase->save();
+
+}
+
+if(array_key_exists('add', $_POST)){
+	include 'View/add.php';
+}else{
+	include 'View/List.php';
+}
+
+
 ?>
-<html><body>
-Hello World!
-</body></html>
