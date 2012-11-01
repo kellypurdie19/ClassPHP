33813e01dd76a9f31327a6d5c59681f9b6d567d1
Kelly <Kelly@192.168.0.101>
2012-11-01 09:11:03 -0400

    added images

---
+++ mysite/views/designernav.php
@@ -0,0 +1,8 @@
+<div id="designernav">
+	<form action="." method="get">
+		<ul id="list">
+			<li id="designer"><a href="?page=designer">Printing</a></li>
+			<li id="photography"><a href="?page=photography">Photography</a></li>
+		</ul>
+	</form>
+</div>
---
+++ mysite/views/photography.php
@@ -0,0 +1,21 @@
+<div id="photography">	
+	<div id="left">
+		<div id="span"></div>
+		<div class="top" >
+			<article></article><h1>Design & Photography</h1><article></article>
+		 	<h2><?php include 'designernav.php'; ?></h2><article id="article"></article>
+		 </div>
+		 
+		 <div id="span"></div>
+	</div>
+	<div id="right">	 
+		<div id="container">
+			<div class="image-container">
+			  <a href="images/Access.jpg" rel="lightbox[roadtrip]" title="Access Database Image"><img src="images/Access.jpg" alt="Access database sample"  width="150" height="150" /></a>
+			  <a href="images/folderOfSchema.jpg" rel="lightbox[roadtrip]" title="Folder schema image"><img src="images/folderOfSchema.jpg" alt="Folder schema sample"  width="150" height="150" /></a>
+			  <a href="images/workbench.jpg" rel="lightbox[roadtrip]" title="Workbench Database image"><img src="images/workbench.jpg" alt="Workbench database sample"  width="150" height="150" /></a>
+			  <a href="images/ConceptialDesign.jpg" rel="lightbox[roadtrip]" title="Conceptual Design image"><img src="images/ConceptialDesign.jpg" alt="Conceptial Design sample"  width="150" height="150" /></a>
+			</div>
+	   	</div> 
+	</div>
+</div>
