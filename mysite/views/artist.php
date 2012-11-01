1ccc506345ceb28c33733d92f3f5c7f797f1fa96
Kelly <Kelly@192.168.0.101>
2012-11-01 12:54:27 -0400

    hate eclipse

--- mysite/views/artist.php
+++ mysite/views/artist.php
@@ -2,10 +2,11 @@
 	<div id="left">
 		<div id="container">
 			<div class="image-container">
-				<a class="fancybox-button" rel="fancybox-button" href="images/butterflyL.jpg" title="An invitation  Factory."><img src="images/butterfly.jpg" alt="" width="150" height="150"/></a>
-				<a class="fancybox-button" rel="fancybox-button" href="images/concordia.jpg" title="Brasted Kent"><img src="images/concord.jpg" alt="" width="150" height="150"/></a>
-				<a class="fancybox-button" rel="fancybox-button" href="images/painting.jpg" title="Brasted Kent"><img src="images/cSharp1.jpg" alt="" width="150" height="150"/></a>
-				<a class="fancybox-button" rel="fancybox-button" href="images/woman.jpg" title="Brasted Kent"><img src="images/cSharp2.jpg" alt="" width="150" height="150"/></a>
+				<a class="fancybox-button" rel="fancybox-button" href="images/butterflyL.jpg" title=""><img src="images/butterfly.jpg" alt="" width="150" height="150"/></a>
+				<a class="fancybox-button" rel="fancybox-button" href="images/concordia.jpg" title=""><img src="images/concord.jpg" alt="" width="150" height="150"/></a>
+				<a class="fancybox-button" rel="fancybox-button" href="images/painting.jpg" title=""><img src="images/cSharp1.jpg" alt="" width="150" height="150"/></a>
+				<a class="fancybox-button" rel="fancybox-button" href="images/woman.jpg" title=""><img src="images/cSharp2.jpg" alt="" width="150" height="150"/></a>
+				<a class="fancybox-button" rel="fancybox-button" href="images/drawings.jpg" title="">
 			</div>
 	   	</div>
 	</div>
--- mysite/views/designer.php
+++ mysite/views/designer.php
@@ -16,12 +16,12 @@
 				 maker. This invitation was made available at art stores across the Mississauga and Oakville area, 
 				 therefore I feel confident that I have not breached any contractual obligations that I may have 
 				 signed for at Image Factory."><img src="images/portfoliopg2.png" alt="" width="150" height="150"/></a>
-				<a class="fancybox-button" rel="fancybox-button" href="images/postcard.jpg" title="Brasted Kent"><img src="images/portfoliopg5.png" alt="" width="150" height="150"/></a>
-				<a class="fancybox-button" rel="fancybox-button" href="images/gmp.jpg" title="Brasted Kent"><img src="images/portfoliopg3.png" alt="" width="150" height="150"/></a>
-				<a class="fancybox-button" rel="fancybox-button" href="images/logo.jpg" title="Brasted Kent"><img src="images/portfoliopg4.png" alt="" width="150" height="150"/></a>
-				<a class="fancybox-button" rel="fancybox-button" href="images/invites.jpg" title="Brasted Kent"></a>
-				<a class="fancybox-button" rel="fancybox-button" href="images/gap.jpg" title="Brasted Kent"></a>
-				<a class="fancybox-button" rel="fancybox-button" href="images/gapella.jpg" title="Brasted Kent"></a>
+				<a class="fancybox-button" rel="fancybox-button" href="images/postcard.jpg" title="POSTCARD"><img src="images/portfoliopg5.png" alt="" width="150" height="150"/></a>
+				<a class="fancybox-button" rel="fancybox-button" href="images/gmp.jpg" title="E-FLYER AND SCHOOL ORDER FORM"><img src="images/portfoliopg3.png" alt="" width="150" height="150"/></a>
+				<a class="fancybox-button" rel="fancybox-button" href="images/logo.jpg" title="LOGO CREATIONS"><img src="images/portfoliopg4.png" alt="" width="150" height="150"/></a>
+				<a class="fancybox-button" rel="fancybox-button" href="images/invites.jpg" title="INVITATIONS"></a>
+				<a class="fancybox-button" rel="fancybox-button" href="images/gap.jpg" title="ADD SAMPLE.1"></a>
+				<a class="fancybox-button" rel="fancybox-button" href="images/gapella.jpg" title="ADD SAMPLE.2"></a>
 			</div>
 	   	</div> 
 	</div>
